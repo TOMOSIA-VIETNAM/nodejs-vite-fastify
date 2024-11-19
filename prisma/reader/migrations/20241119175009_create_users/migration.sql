6b960dc97b116043439a8f03ae627b78044b861b
Minh Tang Q <minh.tang1@tomosia.com>
2024-11-20 01:10:22 +0700

    Split folder reader & writer

--- prisma/reader/migrations/20241119175009_create_users/migration.sql
+++ prisma/reader/migrations/20241119175009_create_users/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `posts` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "posts";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
